e1e1d8d851aa8faed9931adc1167e36456414032
khangtictoc <tranhoangkhang09112001@gmail.com>
2024-07-16 17:38:27 +0000

    Done Deploy

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Check if an environment variable exists
-if os.environ.get('VARIABLE_NAME') == None:
-    print("The environment variable does not exist.")
-else:
-    print("The environment variable exists.")
